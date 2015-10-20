5eeeadf6d7604a65bc4f72a8c4665918e0f97ac0
Aelia Abbas <aelia.abbas@cityrdbclab5015.auckland.natcoll.ac.nz>
2015-10-20 19:16:48 +1300

    1

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// dont put class important on any para, just write it in css
+$(document).ready(function(){
+	$("p").click(function(){
+		$(this).toggleClass("important");
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
